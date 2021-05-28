067ab10e84335d4e1ee875f1738eab281efa42cc
Tristan Tinning <tristan_tinning@hotmail.com>
2021-05-28 14:53:13 +0100

    flask run complete, can use url route to display winner on screen

--- controller/controller.py
+++ controller/controller.py
@@ -7,8 +7,8 @@ from src.player import Player
 def home():
     return render_template('home.html', title="Rock, Paper, Sissors!")
 
-@app.route("choice_1/choice_2")
-def result(choice_1, choice_2):
+@app.route('/<choice_1>/<choice_2>')
+def return_result(choice_1, choice_2):
     player_1 = Player("player_1", choice_1)
     player_2 = Player("player_2", choice_2)
     game = Game(player_1, player_2)
