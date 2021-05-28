30581dce58899f20ebc37a762a5efbdc950a9d85
Tristan Tinning <tristan_tinning@hotmail.com>
2021-05-28 13:44:35 +0100

    game test setup

---
+++ tests/game_test.py
@@ -0,0 +1,22 @@
+import unittest
+from src.player import Player
+from src.game import Game
+
+class TestGame(unittest.TestCase):
+
+    def setUp(self):
+        self.player_1 = Player("Jack Daniels", "paper")
+        self.player_2 = Player("Jim Beam", "rock")
+        self.player_3 = Player("Sailor Jerry", "paper")
+        self.game_1 = Game(self.player_1, self.player_2)
+        self.game_2 = Game(self.player_1, self.player_3)
+
+    def test_game_1_shows_winner(self):
+        expected = "Jack Daniels wins by playing paper"
+        actual = self.game_1.result
+        self.assertEqual(expected, actual)
+
+    def test_game_2_returns_none(self):
+        expected = None
+        actual = self.game_2.result
+        self.assertEqual(expected, actual)
