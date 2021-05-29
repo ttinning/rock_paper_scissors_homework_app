bf1bc255d05adb76e2aef5469848f92dfdd57fb4
Tristan Tinning <tristan_tinning@hotmail.com>
2021-05-29 10:07:14 +0100

    CPU test written

--- src/game.py
+++ src/game.py
@@ -3,6 +3,7 @@ class Game:
     def __init__(self, player_1, player_2):
         self.player_1 = player_1
         self.player_2 = player_2
+        
 
     
     def player_win_output(self, player):
--- tests/game_test.py
+++ tests/game_test.py
@@ -8,8 +8,10 @@ class TestGame(unittest.TestCase):
         self.player_1 = Player("Jack Daniels", "paper")
         self.player_2 = Player("Jim Beam", "rock")
         self.player_3 = Player("Sailor Jerry", "paper")
+        self.player_4 = Player(check_for_cpu_player())
         self.game_1 = Game(self.player_1, self.player_2)
         self.game_2 = Game(self.player_1, self.player_3)
+        self.game_2 = Game(self.player_1, self.player_4)
 
     def test_game_1_shows_winner(self):
         expected = "Jack Daniels wins by playing paper"
@@ -17,6 +19,11 @@ class TestGame(unittest.TestCase):
         self.assertEqual(expected, actual)
 
     def test_game_2_returns_none(self):
+        expected = None
+        actual = self.game_2.result()
+        self.assertEqual(expected, actual)
+
+    def test_cpu_is_available(self):
         expected = None
         actual = self.game_2.result()
         self.assertEqual(expected, actual)
